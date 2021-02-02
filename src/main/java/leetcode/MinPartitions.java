50bd10037800c7e39dbd00126fa653d8ae4bfbb4
liquidjoo <liquidjoo@gmail.com>
2021-02-02 23:28:28 +0900

    feat : leetcode GroupThePeople

---
+++ src/main/java/leetcode/MinPartitions.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class MinPartitions {
+
+    public static void main(String[] args) {
+        final MinPartitions minPartitions = new MinPartitions();
+        final int i = minPartitions.minPartitions("2748938938938");
+        System.out.println(i);
+    }
+
+    public int minPartitions(String n) {
+        return 0;
+    }
+}
