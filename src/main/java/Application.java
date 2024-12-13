03900461269eebac037653810ed4447bb591053c
liquidjoo <liquidjoo@gmail.com>
2024-12-13 11:34:00 +0900

    feat : init

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,9 @@
-import deprecated.slipp.week12.FinalPrices;
+
 
 public class Application {
 
     public static void main(String[] args) {
-//        PathSum2 pathSum2 = new PathSum2();
-//        pathSum2.test();
 
-        FinalPrices finalPrices = new FinalPrices();
-        finalPrices.test();
     }
 
 }
